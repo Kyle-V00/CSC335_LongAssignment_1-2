38607594fc22a7a936c091c1222db5f46d7ae719
liammnorris <148369713+liammnorris@users.noreply.github.com>
2025-02-27 14:26:43 -0700

    Update MusicStore.java: added getAlbum method

--- MusicStore.java
+++ MusicStore.java
@@ -21,11 +21,30 @@ public class MusicStore {
 		return "";
 	}
 
-	public String[] searchSongTitleAndArtist(String title, String artist) {
-		// TODO Auto-generated method stub
-		
+		public String[] searchSongTitleAndArtist(String title, String artist) {
 		// Return String[title, artist, album]
 		// Return null if not found.
+		for (int i = 0; i < albums.size(); i ++) {
+			if (albums.get(i).containsSong(title) && albums.get(i).getArtist().equals(artist)) {
+				String[] info = new String[3];
+				info[0] = title;
+				info[1] = artist;
+				info[2] = albums.get(i).getName();
+				return info;
+			}
+		}
+		return null;
+	}
+
+	public Album getAlbum(String title, String artist) {
+		// TODO Auto-generated method stub
+		// return null if album not found
+		// Otherwise return a copy of the album object
+		for (int i = 0; i < albums.size(); i ++) {
+			if (albums.get(i).getName().equals(title) && albums.get(i).getArtist().equals(artist)) {
+				return albums.get(i).copy();
+			}
+		}
 		return null;
 	}
 
