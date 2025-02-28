5e76cff7daa1bf9b0b67db45be85c726ad847ccf
liammnorris <148369713+liammnorris@users.noreply.github.com>
2025-02-28 15:53:54 -0700

    Small errors fixed

--- view.java
+++ view.java
@@ -4,7 +4,7 @@
  * Purpose: The view to our virtual music library,
  * it handles the user input and directs them with options.
  * 
- * Note: ended up coding this by hand and not Ai. Since the direction we went had some special nuances 
+ * Note: ended up coding most of this by hand and not Ai. Since the direction we went had some special nuances 
  * that made training the Ai too tedious.
  */
 
@@ -20,6 +20,7 @@ public class view {
         lm = new LibraryModel();
     	ms = new MusicStore();
     	userInput();
+
     }
 
     public String userInput() {
@@ -72,41 +73,70 @@ public class view {
     }
     
     private void searchMenu() {
-    	System.out.print("\n");
-    	System.out.print("Menu Options:\n");
-		System.out.print("'s' = search store, 'l' = search library, 'b' = back\n");
-		String nxtCmd = scanner.nextLine();
-    	while (nxtCmd != "b"){
-    		System.out.print("Menu Options:\n");
+    	String nxtCmd = scanner.nextLine();
+		do {
+			System.out.print("Menu Options:\n");
+
     		System.out.print("'s' = search store, 'l' = search library, 'b' = back\n");
-    		if (nxtCmd == "s") {
+    		nxtCmd = scanner.nextLine(); 
+    		if (nxtCmd.equals("s")) {
     			searchStore();
     		}
-    	}
+    		// TODO: need to finish this
+    		if (nxtCmd.equals("l")) {
+    			searchStore();
+    		} 
+    	} while(!scanner.nextLine().toLowerCase().equals("b"));
     	return;
 	}
 
 	private void searchStore() {
-		System.out.print("\n");
-    	System.out.print("Search Store Options:\n");
-		System.out.print("'st' = search song title, 'sa' = search song "
-						+"artist,\n 'at' = search album title, "
-						+"'aa' = search album artist, 'b' = back\n");
-    	String nxtCmd = scanner.nextLine();
-    	while (nxtCmd != "b"){
+//		System.out.print("\n");
+//    	System.out.print("Search Store Options:\n");
+//		System.out.print("'st' = search song title, 'sa' = search song "
+//						+"artist,\n 'at' = search album title, "
+//						+"'aa' = search album artist, 'b' = back\n");
+		String nxtCmd = scanner.nextLine();
+    	do {
     		System.out.print("Search Store Options:\n");
     		System.out.print("'st' = search song title, 'sa' = search song "
     						+"artist,\n 'at' = search album title, "
     						+"'aa' = search album artist, 'b' = back\n");
     		if (nxtCmd.equals("st")) {
-//    			searchStore();
+    			System.out.print("song name: ");
+    			String songName = scanner.nextLine();
+    			String val = lm.storeSearchSongTitle(songName);
+    			if (val.equals(null)) {
+    				System.out.printf("%s not found in MusicStore\n", songName);
+    			}
+    			else {
+//    				System.out.printf("%s's songs: %s", songName,val);
+    				System.out.printf("%s",val);
+    			}
     		}
     		if (nxtCmd.equals("sa")) {
-    			System.out.print("Type artist's name:\n");
-    			nxtCmd = scanner.nextLine();
-    			ms.searchAlbumArtist(nxtCmd);
+    			System.out.print("Type artist's name: ");
+    			String artistName = scanner.nextLine();
+    			String val = lm.storeSearchSongArtist(artistName);
+    			if (val.equals(null)) {
+    				System.out.printf("%s not found in MusicStore\n", artistName);
+    			}
+    			else {
+    				System.out.printf("%s's songs: %s", artistName,val);
+    			}
     		}
-    	}
+    		if (nxtCmd.equals("at")){
+    			System.out.print("Type album title: ");
+    			String albumTitle = scanner.nextLine();
+    			String val = lm.storeSearchAlbumTitle(albumTitle);
+    			if (val.equals(null)) {
+    				System.out.printf("%s not found in MusicStore\n", albumTitle);
+    			}
+    			else {
+    				System.out.printf(val);
+    			}
+    		}
+    	} while (!nxtCmd.equals("b"));
     	return;
 	}
 
